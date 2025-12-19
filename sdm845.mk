19ca7928a90cb99e33db653139f04d02f0506420
Luoxue <luoxue520owo@qq.com>
2025-12-20 05:00:10 +0800

    Update sdm845.mk

--- sdm845.mk
+++ sdm845.mk
@@ -180,6 +180,9 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0.vendor \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm-service.clearkey \
     libcrypto_shim.vendor
