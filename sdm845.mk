4b6d237c72680190e7045103f3ef039fe3f2d2d1
Luoxue <luoxue520owo@qq.com>
2025-12-20 06:34:46 +0800

    Update sdm845.mk

--- sdm845.mk
+++ sdm845.mk
@@ -323,9 +323,9 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
     android.hardware.power@1.2.vendor \
-    libqti-perfd-client
+    vendor.qti.hardware.perf@2.2-service \
+    libqti-perfd-client.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -380,7 +380,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
     hardware/google/pixel \
-    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
     vendor/qcom/opensource/usb/etc
