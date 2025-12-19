4847b61d4b1a771e533f3a3f0e1856000130fb31
Luoxue <luoxue520owo@qq.com>
2025-12-20 04:08:37 +0800

    rm audio 7.0 7.1

--- sdm845.mk
+++ sdm845.mk
@@ -79,13 +79,10 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@4.0-impl \
     android.hardware.audio@5.0-impl \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio@7.0-impl \
-    android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@6.0-impl \
-    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl \
     audio.r_submix.default \
